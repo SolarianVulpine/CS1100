a15cd01ecece51a635d7001a9507045baa3038de
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-09-27 20:40:14 +0000

    added comments on capitalfirstletter.c back in

--- online_store_assignment/capitalfirstletter.c
+++ online_store_assignment/capitalfirstletter.c
@@ -12,6 +12,27 @@
 
 char* _capitalize(char* str)
 {
+
+   // size_t len = strlen(str);
+    // char* newStr = malloc(len + 1);
+
+    // if (!newStr) 
+    // {
+    //     return NULL; // Memory allocation failed
+    // }
+    // char str[20];
+    // int i;
+    // int string_length;
+
+    // printf("Enter a word in lowercase to capitalize the first letter: ");
+    // scanf("%s", str);
+
+    // if (str[0] != '\0') 
+    // {
+    //   char *str[0] = toupper(str[0]);
+    // }
+
+    // printf("Capitalized word: %s\n", str);
     if (str == NULL || *str == '\0') 
     {
         return str; // Return original string if it's NULL or empty
