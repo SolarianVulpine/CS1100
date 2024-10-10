c5ae445ce530ffc5b591f7767d50d1c752bfb750
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-10-10 23:20:49 +0000

    started work on parsing activity

---
+++ Parsing_Primitive/Program.cs
@@ -0,0 +1,5 @@
+﻿/*this in an incompatible type. Will not implicitly convert int to string*/
+// int test = Console.ReadLine();
+
+Console.Write("Enter an integer: ");
+int num1 = int.Parse(Console.ReadLine());
