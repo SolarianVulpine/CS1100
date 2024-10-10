dee49998f4a6132fc705db41f3d1e6c750c4cbe1
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-10-10 21:02:24 +0000

    Created practice C# projects Hello World, RandomType, and SnakeEyes

---
+++ RandomType/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rand = new Random();
+
+int randomNumber = rand.Next();
+Console.WriteLine(randomNumber);
+
+randomNumber = rand.Next(1, 11);
+Console.WriteLine(randomNumber);
+
+if(randomNumber % 2 == 0)
+{
+        Console.WriteLine($"{randomNumber} is even.");
+}
+else
+{
+        Console.WriteLine($"{randomNumber} is odd.");
+}
---
+++ SnakeEyes/Program.cs
@@ -0,0 +1,19 @@
+﻿Random rand = new Random();
+
+int randomNum_A = rand.Next(1, 6);
+int randomNum_B = rand.Next(1, 6);
+Console.WriteLine($"You Rolled {randomNum_A},{randomNum_B}");
+
+int total_Roll = (randomNum_A+randomNum_B);
+if(total_Roll == 2)
+{
+        Console.WriteLine("Snake Eyes!");
+}
+else if(total_Roll == 12)
+{
+        Console.WriteLine("Box Cars!");
+}
+else
+{
+        Console.WriteLine($"{total_Roll}");
+}
