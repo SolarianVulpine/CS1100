38d468c36cdac3f1abfa8139c90b4d7b9a25d74b
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-10-17 01:12:54 +0000

    created and ran multiplication table, Full points on first vocareum submit! Celebration to occur

--- Area_Calculator/obj/Debug/net8.0/Area_Calculator.AssemblyInfo.cs
+++ Area_Calculator/obj/Debug/net8.0/Area_Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Area_Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29257a2240b6ce19f898d19a0b46e5127c05e64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791473458ebee6a2746b71461a6f73e7c7c88638")]
 [assembly: System.Reflection.AssemblyProductAttribute("Area_Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Area_Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Multiplication_table/Program.cs
@@ -0,0 +1,50 @@
+﻿// prompt user
+Console.Write("Enter starting value for rows: ");
+string startRows_Input = Console.ReadLine();
+
+Console.Write("Enter ending value for rows: ");
+string endRows_Input = Console.ReadLine();
+
+Console.Write("Enter starting value for columns: ");
+string startColumns_Input = Console.ReadLine();
+
+Console.Write("Enter ending value for columns: ");
+string endColumns_Input = Console.ReadLine();
+
+// parse value
+int startRows = int.Parse(startRows_Input);
+int endRows = int.Parse(endRows_Input);
+int startColumns = int.Parse(startColumns_Input);
+int endColumns = int.Parse(endColumns_Input);
+
+// begin loops
+for(int j = startColumns; j <= endColumns; j++)
+{
+        Console.Write($"\t{j}");
+}
+
+Console.WriteLine();
+
+for(int j = startColumns; j <= endColumns; j++)
+{
+        Console.Write("==========");
+}
+
+Console.WriteLine();
+
+for(int i = startRows; i <= endRows; i++)
+{
+        Console.Write($"{i}\t");
+    for(int j = startColumns; j <= endColumns; j++)
+    {
+        Console.Write($"{j * i}\t");
+    }
+    Console.WriteLine();
+}
+
+// Console.WriteLine();
+
+// for(int j = startColumns; j <= endColumns; j++)
+// {
+//         Console.Write("==========");
+// }
