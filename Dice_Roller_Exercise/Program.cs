48a8fe9cd1174ba392faa20f7310bd5199ab7a8d
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-10-19 21:24:05 +0000

    set up a foreach loop that converts each split portion of the CSV string input array from string to ints

--- Dice_Roller_Exercise/Program.cs
+++ Dice_Roller_Exercise/Program.cs
@@ -24,34 +24,65 @@ diceRolls[4] = diceFive;
 //         Console.WriteLine($"i = {i}\t random roll was {diceRolls[i]}");
 // }
 
-Console.WriteLine("Initial Roll: ");
-int index = 1;
-foreach (int Roll in diceRolls)
-{
-        Console.Write($"Die {index}: ");
-        Console.WriteLine(Roll);
-        index++;
-}
+
 
 
 // Declare variable to count roll attempts
-// int numRolls = 1;
+int numRolls = 1;
 
-// while (numRolls < 3)
-// {
-//         if (numRolls == 1)
-//         {
-//                 Console.WriteLine("Initial Roll: ");
-//                 int index = 1;
-//                 foreach (int Roll in diceRolls)
-//                 {
-//                         Console.Write($"{index}: ");
-//                         Console.WriteLine(Roll);
-//                         index++;
-//                 }
-//         }
-//         else
-//         {
-//                 Console.WriteLine("Updated Rolls: ");
-//         }
-// }
+while (numRolls < 4)
+{
+        if (numRolls == 1)
+        {
+                Console.WriteLine("Initial Roll: ");
+                int index = 1;
+                foreach (int Roll in diceRolls)
+                {
+                        Console.Write($"Die {index}: ");
+                        Console.WriteLine(Roll);
+                        index++;
+                }
+                numRolls++;
+                // Console.WriteLine("Enter the dice numbers to reroll (comma-separated, ex: 2,3,5) or leave blank to keep current dice.");
+        }
+        else
+        {
+                Console.WriteLine("Enter the dice numbers to reroll (comma-separated, ex: 2,3,5) or leave blank to keep current dice.");
+                string reroll_Input = Console.ReadLine();
+
+                switch (reroll_Input)
+                {
+                        case null:
+                                break;
+                        case "":
+                                break;
+                        case string s when !string.IsNullOrEmpty(s):
+                                string[] dice_to_reroll = reroll_Input.Split(',');
+                                int index = 1;
+                                foreach (string dice in dice_to_reroll)
+                                {
+                                        int index_dice_to_reroll = int.Parse(dice);
+                                        Console.WriteLine(index_dice_to_reroll);
+                                        
+                                }
+
+                                // foreach (int Roll in diceRolls)
+                                // {
+                                //         Console.Write($"Die {index}: ");
+                                //         Console.WriteLine(Roll);
+                                //         index++;
+                                // }
+                                break;
+                                default:
+                                break;
+
+
+                }
+                Console.WriteLine("made it here\nbreaking");
+                break;
+        }
+}
+
+// Console.WriteLine("\nmade it here!");
+//Console.WriteLine("Breaking~");
+//break;
--- Dice_Roller_Exercise/obj/Debug/net8.0/Dice_Roller_Exercise.AssemblyInfo.cs
+++ Dice_Roller_Exercise/obj/Debug/net8.0/Dice_Roller_Exercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dice_Roller_Exercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d1e037b2a9ca6c66e1e8c000dda588a4497c5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7ebd3ff1371c652e4aedd504ed5ed2e92a9d4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dice_Roller_Exercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dice_Roller_Exercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
